bea20ed3bec4660ac39e139adbb9e26cb848907b
rokib2000 <rokib0w1@gmail.com>
2022-10-24 23:54:39 +0600

    project Setup

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.listen(port, () => {
+  console.log(`Edu Solutions running port, ${port}`);
+});
