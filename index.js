7806692446415e88be7ec1f54f41380535f2f83e
rokib2000 <rokib0w1@gmail.com>
2022-10-25 00:49:05 +0600

    vercel json and data fetch

--- index.js
+++ index.js
@@ -6,8 +6,27 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const courses = require("./data/courses.json");
+
 app.get("/", (req, res) => {
   res.send("Server running  successfully");
+});
+
+//get courses data json file
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+//get course data json file
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const course = courses.find((c) => c.id === id);
+
+  if (!course) {
+    res.send("Data Not found");
+  } else {
+    res.send(course);
+  }
 });
 
 app.listen(port, () => {
